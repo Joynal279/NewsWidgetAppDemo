bd8f1ee34f0ae483239ff28a38cd8b9af36854a4
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-08 12:55:19 +0600

    modify NewsViewModel classes

---
+++ NewsWidget/NewsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetBundle.swift
+//  NewsWidget
+//
+//  Created by Joynal Abedin on 8/9/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidget()
+    }
+}
--- NewsWidgetAppDemo/NewsViewModel.swift
+++ NewsWidgetAppDemo/NewsViewModel.swift
@@ -16,64 +16,66 @@ final class NewsViewModel: ObservableObject {
       static let apiKey = "09510d0f4f3641258f6f703aa2ef9612"
     }
 
+    // MARK: - properties
     enum Category: String {
-       case general
-       case business
-       case entertainment
-       case health
-       case science
-       case sports
-       case technology
-   }
+        case general
+        case business
+        case entertainment
+        case health
+        case science
+        case sports
+        case technology
+    }
 
-    // MARK: - state
     enum ResultState {
         case loading
         case error(Error)
         case success([NewsArticle])
     }
 
-    // MARK: - properties
     private var cancelables: Set<AnyCancellable> = Set<AnyCancellable>()
     private let apiService: APIService
 
     @Published var state: ResultState = .loading
 
+    // MARK: - init
     init(apiService: APIService = APIService() ) {
         self.apiService = apiService
     }
 
-    func getDataIfNeeded(category: Category = .general) {
-         guard let url = getUrlForCategory(category: category) else {
-             //completion?(.failure(APIService.APIError.unknownError))
-             return
-         }
+    // MARK: - get data
+    func getDataIfNeeded(category: Category = .general, completion: ((Result<[NewsArticle], Error>) -> Void )? = nil ) {
+        guard let url = getUrlForCategory(category: category) else {
+            completion?(.failure(APIService.APIError.unknownError))
+            return
+        }
 
-         state = .loading
-         apiService.getObject(object: NewsArticleList.self, url: url)
-             .subscribe(on: DispatchQueue.global(qos: .background))
-             .receive(on: DispatchQueue.main)
-             .sink { [weak self] result in
-                 switch result {
-                 case .failure(let error):
-                     self?.state = .error(error)
-                     //completion?(.failure(error))
-                 default: break
-                 }
+        state = .loading
+        apiService.getObject(object: NewsArticleList.self, url: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] result in
+                switch result {
+                case .failure(let error):
+                    self?.state = .error(error)
+                    completion?(.failure(error))
+                default: break
+                }
 
-             } receiveValue: { [weak self] list in
-                 self?.state = .success(list.articles)
-             }.store(in: &cancelables)
-     }
+            } receiveValue: { [weak self] list in
+                self?.state = .success(list.articles)
+                completion?(.success(list.articles))
+            }.store(in: &cancelables)
+    }
 
-     private func getUrlForCategory(category: Category = .general) -> URL? {
-          var urlComponents = URLComponents(string: Constants.endpointString)
-          var queryItems = urlComponents?.queryItems
-          queryItems?.append(URLQueryItem(name: "category", value: category.rawValue))
-          queryItems?.append(URLQueryItem(name: "apiKey", value: Constants.apiKey))
-          urlComponents?.queryItems = queryItems
+    private func getUrlForCategory(category: Category = .general) -> URL? {
+         var urlComponents = URLComponents(string: Constants.endpointString)
+         var queryItems = urlComponents?.queryItems
+         queryItems?.append(URLQueryItem(name: "category", value: category.rawValue))
+         queryItems?.append(URLQueryItem(name: "apiKey", value: Constants.apiKey))
+         urlComponents?.queryItems = queryItems
 
-          return urlComponents?.url
-     }
+         return urlComponents?.url
+    }
 
 }
