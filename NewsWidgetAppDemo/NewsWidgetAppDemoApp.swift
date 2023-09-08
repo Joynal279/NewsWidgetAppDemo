5979d247c46433111758a1a8dc34ae00f7c33abf
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-08 08:11:00 +0600

    Initial commit

---
+++ NewsWidgetAppDemo/NewsWidgetAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWidgetAppDemoApp.swift
+//  NewsWidgetAppDemo
+//
+//  Created by Joynal Abedin on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsWidgetAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
